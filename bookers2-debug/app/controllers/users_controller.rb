3b1d01e3571a2cc6113c11617992a1751ed9983d
konamarii <chankonworld@gmail.com>
2021-07-24 13:14:21 +0000

    [finish]bookers2-debug

--- bookers2-debug/app/controllers/users_controller.rb
+++ bookers2-debug/app/controllers/users_controller.rb
@@ -9,29 +9,37 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    @user = current_user
+    @books = Book.all
     @book = Book.new
+  end
 
   def edit
     @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(current_user)
+    end
   end
 
   def update
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to user_path(@user.id), notice: "You have updated user successfully."
     else
-      render "show"
+      render :edit
+       @user = User.find(params[:id])
     end
-  end
-
-  private
-  def user_params
-    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 
   def ensure_correct_user
     @user = User.find(params[:id])
-    unless @user == current_user
+    if @user != current_user
       redirect_to user_path(current_user)
     end
   end
+
+   private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
 end
